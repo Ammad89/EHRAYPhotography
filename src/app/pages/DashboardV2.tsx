61c706a998497445b07bb38c431b751533db947e
root <root@srv1390696.hstgr.cloud>
2026-06-22 23:38:22 +0000

    Fix dashboard v2 editor panels

--- src/app/pages/DashboardV2.tsx
+++ src/app/pages/DashboardV2.tsx
@@ -2,9 +2,9 @@ import CmsShell from "../../cms-core/dashboard/CmsShell";
 import PagesPanel from "../../cms-core/dashboard/PagesPanel";
 import BlocksPanel from "../../cms-core/dashboard/BlocksPanel";
 import FieldsPanel from "../../cms-core/dashboard/FieldsPanel";
+import PreviewRenderer from "../../cms-core/dashboard/PreviewRenderer";
 import ThemePanel from "../../cms-core/dashboard/ThemePanel";
 import SiteSettingsPanel from "../../cms-core/dashboard/SiteSettingsPanel";
-import PreviewRenderer from "../../cms-core/dashboard/PreviewRenderer";
 import { useCmsEditor } from "../../cms-core/dashboard/useCmsEditor";
 
 export default function DashboardV2() {
@@ -29,21 +29,28 @@ export default function DashboardV2() {
           />
         </div>
       }
+      editor={
+        <div>
+          <FieldsPanel
+            block={editor.selectedBlock}
+            onChangeField={editor.updateBlockField}
+          />
 
-        editor={
-           <div>
-            <FieldsPanel
-                 block={editor.selectedBlock}
-                 onChangeField={editor.updateBlockField}
+          <div className="border-t border-border mt-6">
+            <ThemePanel
+              theme={editor.theme}
+              onChange={editor.setTheme}
             />
-             <div className="border-t border-border mt-6">
-             <SiteSettingsPanel
-                settings={editor.siteSettings}
-                onChange={editor.setSiteSettings}
+          </div>
+
+          <div className="border-t border-border mt-6">
+            <SiteSettingsPanel
+              settings={editor.siteSettings}
+              onChange={editor.setSiteSettings}
             />
-           </div>
-           </div>
-             }
+          </div>
+        </div>
+      }
       preview={
         <PreviewRenderer
           page={editor.selectedPage}
